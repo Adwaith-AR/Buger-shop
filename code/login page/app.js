310fc64f9a850bfd9f8de55e138f83c592703bda
Adwaith-AR <adwaith.rakhy.office@gamil.com>
2026-05-20 00:58:01 +0530

    login page fainalised

---
+++ code/login page/app.js
@@ -0,0 +1,8 @@
+let username; 
+window.alert(go);
+document.getElementById("button").onclick = function(){
+          username = document.getElementById("userName").ariaValueMax;
+          window.alert(go);
+          console.log("padi mone")
+          
+} 
