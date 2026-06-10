596ed3149089a65d4d2eaa9c2dd48c6d9f965f11
Adwaith-AR <adwaith.rakhy.office@gamil.com>
2026-06-10 23:57:04 +0530

    burger coustmasation test

--- code/orderind page/BBQ Chicken Burger/app.js
+++ code/orderind page/BBQ Chicken Burger/app.js
@@ -6,6 +6,8 @@ const CartBtn = document.getElementById("b1");
 const OrderBtn = document.getElementById("b2");
 const deleteBtn = document.getElementById("deleteBtn");
 const productQuantityContainer = document.getElementById("product_quantity");
+const burgerContainer = document.getElementById("bcontainer");
+
 
 
 const burger = document.getElementById("burger");
@@ -41,15 +43,22 @@ const cost = document.getElementById("cost");
 const lost = document.getElementById("lost");
 const clost = document.getElementById("clost");
 
-
-
 const C = document.getElementById("chess");
 const O = document.getElementById("onions");
 const T = document.getElementById("tomatoes");
 const L = document.getElementById("lettuce");
 const S = document.getElementById("ketchup");
 
-let item_selected = clost;
+C.checked = true
+O.checked = true
+T.checked = true
+L.checked = true
+S.checked=true
+
+let items = clost;
+
+
+
 
 
 function hideBurger() {
@@ -86,13 +95,369 @@ function hideBurger() {
           lost.classList.add("hide");
           clost.classList.add("hide");
           burger.classList.add("hide");
-          
-          
+
 
 }
-hideBurger()
+hideBurger();
 burger.classList.remove("hide");
 
+function burger_to_show(item_selected) {
+          hideBurger()
+          if (item_selected == "") {
+                    hideBurger();
+                    burger.classList.remove("hide");
+          }
+          else if (item_selected.includes("c")
+                    && item_selected.includes("l") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    c.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    l.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    o.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    s.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    t.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    cl.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    co.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    cs.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    ct.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    lo.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    ls.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    lt.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    os.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    ot.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    st.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    clo.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    cls.classList.remove("hide");
+          } else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    clt.classList.remove("hide");
+          } else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    cos.classList.remove("hide");
+          } else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    cot.classList.remove("hide");
+          } else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    cst.classList.remove("hide");
+          } else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    los.classList.remove("hide");
+          } else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    lot.classList.remove("hide");
+          } else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    lst.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    ost.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t") == false
+          ) {
+                    hideBurger();
+                    clos.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s") == false
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    clot.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o") == false
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    clst.classList.remove("hide");
+          }
+          else if (item_selected.includes("l") == false
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    cost.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c") == false
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    lost.classList.remove("hide");
+          }
+          else if (item_selected.includes("l")
+                    && item_selected.includes("c")
+                    && item_selected.includes("o")
+                    && item_selected.includes("s")
+                    && item_selected.includes("t")
+          ) {
+                    hideBurger();
+                    clost.classList.remove("hide");
+          }
+}
+
+function collectData() {
+          items = ""
+          if (C.checked == false) {
+                    items = items + "c"
+          }
+          if (O.checked == false) {
+                    items = items + "o"
+          }
+          if (T.checked == false) {
+                    items = items + "t"
+          }
+          if (L.checked == false) {
+                    items = items + "l"
+          }
+          if (S.checked == false) {
+                    items = items + "s"
+          }
+          burger_to_show(items)
+}
+
+
+
+C.onclick = function () {
+          burgerContainer.classList.add("blur")
+
+          setTimeout(() => {
+                    collectData()
+                    burgerContainer.classList.add("anti_blur")
+                    burgerContainer.classList.remove("blur")
+
+          }, 500);
+          burgerContainer.classList.remove("anti_blur")
+}
+O.onclick = function () {
+          burgerContainer.classList.add("blur")
+
+          setTimeout(() => {
+                    collectData()
+                    burgerContainer.classList.add("anti_blur")
+                    burgerContainer.classList.remove("blur")
+
+          }, 500);
+          burgerContainer.classList.remove("anti_blur")
+}
+T.onclick = function () {
+          burgerContainer.classList.add("blur")
+
+          setTimeout(() => {
+                    collectData()
+                    burgerContainer.classList.add("anti_blur")
+                    burgerContainer.classList.remove("blur")
+
+          }, 500);
+          burgerContainer.classList.remove("anti_blur")
+}
+L.onclick = function () {
+          burgerContainer.classList.add("blur")
+
+          setTimeout(() => {
+                    collectData()
+                    burgerContainer.classList.add("anti_blur")
+                    burgerContainer.classList.remove("blur")
+
+          }, 500);
+          burgerContainer.classList.remove("anti_blur")
+}
+S.onclick = function () {
+          burgerContainer.classList.add("blur")
+
+          setTimeout(() => {
+                    collectData()
+                    burgerContainer.classList.add("anti_blur")
+                    burgerContainer.classList.remove("blur")
+
+          }, 300);
+          burgerContainer.classList.remove("anti_blur")
+}
 
 
 
